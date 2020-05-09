3e4915c37fe125931813042bad01e456fca80df9
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2020-05-09 14:33:56 -0400

    Following tutorial steps - heroku deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# Base controller
 class ApplicationController < ActionController::Base
+  def hello
+    render html: '¡hola mundo!'
+  end
+
+  def goodbye
+    render html: 'goodbye!'
+  end
 end
